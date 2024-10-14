cf004fcd1a71fd57b73f7012d4029cb2b43ebefc
sukhbinder singh <sukhbindersingh@sukhMacPro.local>
2024-10-14 21:01:25 +0530

    Fix test

--- tests/test_winzy_text_to_image.py
+++ tests/test_winzy_text_to_image.py
@@ -2,6 +2,7 @@ import pytest
 import winzy_text_to_image as w
 
 from argparse import Namespace, ArgumentParser
+from unittest.mock import patch, MagicMock
 
 def test_create_parser():
     subparser = ArgumentParser().add_subparsers()
@@ -16,10 +17,11 @@ def test_create_parser():
     assert result.height is None
     assert result.model is None
     assert result.show == False
-    assert result.output == False
+    assert result.output == "generated_image.jpg"
 
 
 def test_plugin(capsys):
     w.txt2img_plugin.hello(None)
     captured = capsys.readouterr()
     assert "Hello! This is an example ``winzy`` plugin." in captured.out
+
