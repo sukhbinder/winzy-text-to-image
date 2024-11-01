a1f97267e1be5077c20466e705953db1c8d1b3bb
Sukhbinder Singh <sukhbindersingh@gmail.com>
2024-11-01 21:56:19 +0530

    Promt can now be wthout quotes and can be as long the user wants. The output can be a filename or a path, and if not given a random file name is used and created in the called folder

--- tests/test_winzy_text_to_image.py
+++ tests/test_winzy_text_to_image.py
@@ -1,8 +1,7 @@
-import pytest
 import winzy_text_to_image as w
 
-from argparse import Namespace, ArgumentParser
-from unittest.mock import patch, MagicMock
+from argparse import ArgumentParser
+
 
 def test_create_parser():
     subparser = ArgumentParser().add_subparsers()
@@ -10,14 +9,14 @@ def test_create_parser():
 
     assert parser is not None
 
-    result, _ = parser.parse_known_args(['-p', 'hello'])
-    assert result.prompt == "hello"
+    result, _ = parser.parse_known_args(["-p", "hello"])
+    assert result.prompt == ["hello"]
     assert result.seed is None
     assert result.width is None
     assert result.height is None
     assert result.model is None
     assert result.show == False
-    assert result.output == "generated_image.jpg"
+    assert result.output is None
 
 
 def test_plugin(capsys):
@@ -25,3 +24,14 @@ def test_plugin(capsys):
     captured = capsys.readouterr()
     assert "Hello! This is an example ``winzy`` plugin." in captured.out
 
+
+def test_get_random_filename():
+    filename = w.get_random_filename()
+    assert isinstance(filename, str)
+    assert len(filename) > 3
+    assert filename.endswith(".jpg")
+
+
+def test_get_random_filename_ext():
+    filename = w.get_random_filename(ext=".png")
+    assert filename.endswith(".png")
--- winzy_text_to_image/__init__.py
+++ winzy_text_to_image/__init__.py
@@ -1,24 +1,73 @@
 import winzy
 import requests
 import os
+import hashlib
+from datetime import datetime
+
+
+def get_random_filename(ext=".jpg"):
+    now = datetime.now()
+    timestamp = now.strftime("%Y%m%d%H%M%S")
+    filename = hashlib.sha256(timestamp.encode()).hexdigest()[:20] + ext
+    return filename
 
 
 def create_parser(subparser):
-    parser = subparser.add_parser("txt2img", description="Generate text to image using pollinations.ai api")
-    parser.add_argument("-p", "--prompt", type=str, required=True, help="Prompt for the image URL")
-    parser.add_argument("-o", "--output", type=str, default='generated_image.jpg', help="Output filename (default: generated_image.jpg)")
-    parser.add_argument("-w", "--width", type=int, default=None, help="Image Width default None")
-    parser.add_argument("-hi", "--height", type=int, default=None, help="Image height default None")
-    parser.add_argument("-s", "--seed", type=int, default=None, help="Seed to use default None")
-    parser.add_argument("-m", "--model", type=str, default=None, help="Model to use ex: 'flux' default None")
-    parser.add_argument("-sh", "--show", action="store_true", help="If given show the downloaded file")
-    
+    parser = subparser.add_parser(
+        "txt2img", description="Generate text to image using pollinations.ai api"
+    )
+    parser.add_argument(
+        "-p",
+        "--prompt",
+        type=str,
+        nargs="*",
+        required=True,
+        help="Prompt for the image URL",
+    )
+    parser.add_argument(
+        "-o", "--output", type=str, default=None, help="Output filename (default: None)"
+    )
+    parser.add_argument(
+        "-w", "--width", type=int, default=None, help="Image Width default None"
+    )
+    parser.add_argument(
+        "-hi", "--height", type=int, default=None, help="Image height default None"
+    )
+    parser.add_argument(
+        "-s", "--seed", type=int, default=None, help="Seed to use default None"
+    )
+    parser.add_argument(
+        "-m",
+        "--model",
+        type=str,
+        default=None,
+        help="Model to use ex: 'flux' default None",
+    )
+    parser.add_argument(
+        "-sh", "--show", action="store_true", help="If given show the downloaded file"
+    )
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="If given show the downloaded file"
+    )
+
     return parser
 
 
-def download_image(prompt, filename="generated_image.jpg", width=None, height=None, seed=None, model=None, show=False):
+def download_image(
+    prompt,
+    filename=None,
+    width=None,
+    height=None,
+    seed=None,
+    model=None,
+    show=False,
+    verbose=False,
+):
 
-    url = f"https://pollinations.ai/p/{prompt}"
+    if prompt:
+        prompt_str = " ".join(prompt)
+
+    url = f"https://pollinations.ai/p/{prompt_str}"
     if width:
         url += f"?width={width}"
     if height:
@@ -27,11 +76,21 @@ def download_image(prompt, filename="generated_image.jpg", width=None, height=No
         url += f"&seed={seed}"
     if model:
         url += f"&model={model}"
-    
+
     response = requests.get(url)
-    with open(filename, 'wb') as file:
+
+    if verbose:
+        print(response)
+
+    if filename is None:
+        filename = get_random_filename()
+    # if path is provided but no file extention then
+    elif "." not in filename and os.path.exists(filename):
+        filename = os.path.join(filename, get_random_filename())
+
+    with open(filename, "wb") as file:
         file.write(response.content)
-    print(f'Image downloaded to {filename}!')
+    print(f"{filename}")
 
     if show:
         os.startfile(filename)
@@ -39,6 +98,7 @@ def download_image(prompt, filename="generated_image.jpg", width=None, height=No
 
 class HelloWorld:
     """ An example plugin """
+
     __name__ = "txt2img"
 
     @winzy.hookimpl
@@ -47,11 +107,20 @@ class HelloWorld:
         parser.set_defaults(func=self.generate)
 
     def generate(self, args):
-        _ = download_image(args.prompt, filename=args.output, width=args.width, 
-                           height=args.height, seed=args.seed, model=args.model, show=args.show)
+        _ = download_image(
+            args.prompt,
+            filename=args.output,
+            width=args.width,
+            height=args.height,
+            seed=args.seed,
+            model=args.model,
+            show=args.show,
+            verbose=args.verbose,
+        )
 
     def hello(self, args):
         # this routine will be called when "winzy "txt2img is called."
         print("Hello! This is an example ``winzy`` plugin.")
+
 
 txt2img_plugin = HelloWorld()
